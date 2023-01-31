f506002aaa49ed3df1b12f05026cd9c01ca5e40c
mpclifford88 <104406469+mpclifford88@users.noreply.github.com>
2023-01-31 17:40:52 -0600

    Add files via upload

---
+++ mongodb_test.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+
+url = "mongodb+srv://admin:<password>@cluster0.4apea7f.mongodb.net/?retryWrites=true&w=majority",
+client = MongoClient(url),
+db = client.pytech
+
+print("\n -- Pytech Collection List --")
+print(db.list_collection_names)
+
+input("\n\n End of program, press any key to exit...")
