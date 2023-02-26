08ff14d45535269fce163140a199f2b3a8b9ddbf
mpclifford88 <104406469+mpclifford88@users.noreply.github.com>
2023-02-26 13:38:52 -0600

    Update Clifford_assignment_12.py

--- module 12/Clifford_assignment_12.py
+++ module 12/Clifford_assignment_12.py
@@ -1,8 +1,16 @@
+'''
+    Title: what_a_book_.py
+    Author: Matthew Clifford
+    Date: 02/26/2023
+'''
+
+# cspell: disable
+# pylint: disable=consider-using-f-string;
+# pylint: disable=c0103
 
 import sys
 import mysql.connector
 from mysql.connector import errorcode
-
 
 config = {
     "user": "whatabook_user",
@@ -13,6 +21,7 @@ config = {
 }
 
 def show_menu():
+    '''This is to show the main menu'''
     print("\n  -- Main Menu --")
 
     print("    1. View Books\n    2. View Store Locations\n    3. My Account\n    4. Exit Program")
@@ -27,17 +36,17 @@ def show_menu():
         sys.exit(0)
 
 def show_books(_cursor):
+    '''This is the inner join'''
     _cursor.execute("SELECT book_id, book_name, author, details from book")
-
     books = _cursor.fetchall()
 
     print("\n  -- DISPLAYING BOOK LISTING --")
-    
     for book in books:
         print("  Book Name: {}\n  Author: {}\n  Details: {}\n".format(book[0], book[1], book[2]))
 
 def show_locations(_cursor):
-    _cursor.execute("SELECT store_id, locale from store")
+    '''Inner join'''
+    _cursor.execute("SELECT store_id,S locale from store")
 
     locations = _cursor.fetchall()
 
@@ -47,7 +56,7 @@ def show_locations(_cursor):
         print("  Locale: {}\n".format(location[1]))
 
 def validate_user():
-
+    '''validate the ID of users'''
     try:
         user_id = int(input('\n      Enter a customer id <Example 1 for user_id 1>: '))
 
@@ -62,26 +71,26 @@ def validate_user():
         sys.exit(0)
 
 def show_account_menu():
-
+    '''This is the account menu of the user'''
     try:
         print("\n      -- Customer Menu --")
         print("        1. Wishlist\n        2. Add Book\n        3. Main Menu")
-        account_option = int(input('        <Example enter: 1 for wishlist>: '))
+        account_option1 = int(input('        <Example enter: 1 for wishlist>: '))
 
-        return account_option
+        return account_option1
     except ValueError:
         print("\n  Invalid number, program terminated...\n")
 
         sys.exit(0)
 
 def show_wishlist(_cursor, _user_id):
-
-    _cursor.execute("SELECT user.user_id, user.first_name, user.last_name, book.book_id, book.book_name, book.author " + 
+    '''This is where we query the DB for a list of books added to the user's wishlist'''
+    _cursor.execute("SELECT user.user_id, user.first_name, user.last_name" +
+                    "book.book_id, book.book_name, book.author " +
                     "FROM wishlist " + 
                     "INNER JOIN user ON wishlist.user_id = user.user_id " + 
                     "INNER JOIN book ON wishlist.book_id = book.book_id " + 
-                    "WHERE user.user_id = {}".format(_user_id))
-    
+                    "WHERE user.user_id = {}".format(_user_id))  
     wishlist = _cursor.fetchall()
 
     print("\n        -- DISPLAYING WISHLIST ITEMS --")
@@ -94,7 +103,8 @@ def show_books_to_add(_cursor, _user_id):
 
     query = ("SELECT book_id, book_name, author, details "
             "FROM book "
-            "WHERE book_id NOT IN (SELECT book_id FROM wishlist WHERE user_id = {})".format(_user_id))
+            "WHERE book_id NOT IN (SELECT book_id FROM wishlist WHERE user_id = {})"
+            .format(_user_id))
 
     print(query)
 
@@ -108,18 +118,19 @@ def show_books_to_add(_cursor, _user_id):
         print("        Book Id: {}\n        Book Name: {}\n".format(book[0], book[1]))
 
 def add_book_to_wishlist(_cursor, _user_id, _book_id):
-    _cursor.execute("INSERT INTO wishlist(user_id, book_id) VALUES({}, {})".format(_user_id, _book_id))
+    '''defining the command to add a book to the wishlist'''
+    _cursor.execute("INSERT INTO wishlist(user_id, book_id) VALUES({}, {})"
+                    .format(_user_id, _book_id))
 
 try:
-    """ try/catch block for handling potential MySQL database errors """ 
 
-    db = mysql.connector.connect(**config) # connect to the WhatABook database 
+    db = mysql.connector.connect(**config) # connect to the WhatABook database
 
     cursor = db.cursor() # cursor for MySQL queries
 
     print("\n  Welcome to the WhatABook Application! ")
 
-    user_selection = show_menu() # show the main menu 
+    user_selection = show_menu() # show the main menu
 
     while user_selection != 4:
 
@@ -143,22 +154,19 @@ try:
                     show_books_to_add(cursor, my_user_id)
 
                     book_id = int(input("\n        Enter the id of the book you want to add: "))
-                    
                     add_book_to_wishlist(cursor, my_user_id, book_id)
 
                     db.commit()
 
                     print("\n        Book id: {} was added to your wishlist!".format(book_id))
- 
+
                 if account_option < 0 or account_option > 3:
                     print("\n      Invalid option, please retry...")
 
                 account_option = show_account_menu()
-        
         if user_selection < 0 or user_selection > 4:
             print("\n      Invalid option, please retry...")
-            
-        user_selection = show_menu()
+            user_selection = show_menu()
 
     print("\n\n  Program terminated...")
 
@@ -176,3 +184,4 @@ except mysql.connector.Error as err:
 finally:
 
     db.close()
+    
